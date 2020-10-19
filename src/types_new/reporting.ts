b384d879c0274d6f8b21da9c8fff2052b02ee5bf
Daniil Bratukhin <dantothefuture@gmail.com>
2020-10-19 15:02:58 +0300

    feat: add reporting api types

---
+++ src/types_new/reporting.ts
@@ -0,0 +1,22 @@
+import type * as FxReportingStoreDomainExists from "./reporting_store_domain_exists";
+import type * as FxReportingEmailExists from "./reporting_email_exists";
+
+export type Rel = "reporting";
+export type Curie = "fx:reporting";
+export type Methods = "GET" | "HEAD" | "OPTIONS";
+
+export interface Links {
+  /** This resource. */
+  "self": Links;
+  /** Send a GET with an `email` query parameter to see if an existing user exists for this email value. */
+  "fx:reporting_email_exists": FxReportingEmailExists.Links;
+  /** Send a GET with a `store_domain` query parameter to see if an existing store exists for this `store_domain` value. */
+  "fx:reporting_store_domain_exists": FxReportingStoreDomainExists.Links;
+}
+
+export interface Props {
+  /** A small, human readable explanation of this resource. */
+  message: string;
+}
+
+export type Zoom = never;
