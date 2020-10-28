0fb19fb968746f4cb4ab8b891fea6de68591cf22
Daniil Bratukhin <dantothefuture@gmail.com>
2020-10-28 19:30:20 +0300

    wip: move api element to @foxy.io/elements (new branch)

--- src/element/index.ts
+++
@@ -1,6 +0,0 @@
-import { APIElement } from "./APIElement";
-
-customElements.define("foxy-api", APIElement);
-
-export { RequestEvent } from "./RequestEvent";
-export { APIElement } from "./APIElement";
