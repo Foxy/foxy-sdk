489861308c37fce67d2b1220158b6f8d511d6c98
Daniil Bratukhin <dantothefuture@gmail.com>
2020-11-09 19:58:06 +0300

    test: add tests for auth error

---
+++ tests/core/API/AuthError.test.ts
@@ -0,0 +1,35 @@
+import { AuthError } from '../../../src/core/API/AuthError';
+
+describe('Core', () => {
+  describe('API', () => {
+    describe('AuthError', () => {
+      it('exposes error codes as static members', () => {
+        expect(AuthError).toHaveProperty('NEW_PASSWORD_REQUIRED');
+        expect(AuthError).toHaveProperty('UNAUTHORIZED');
+        expect(AuthError).toHaveProperty('UNKNOWN');
+      });
+
+      it('extends Error', () => {
+        const error = new AuthError({ code: AuthError.UNKNOWN });
+        expect(error).toBeInstanceOf(Error);
+      });
+
+      it('stores error code in the .code property', () => {
+        const code = AuthError.UNAUTHORIZED;
+        const error = new AuthError({ code });
+        expect(error).toHaveProperty('code', code);
+      });
+
+      it('stores undefined in the .originalError property when constructed without params.originalError', () => {
+        const error = new AuthError({ code: AuthError.UNKNOWN });
+        expect(error).toHaveProperty('originalError', undefined);
+      });
+
+      it('stores provided value in the .originalError property when constructed with params.originalError', () => {
+        const originalError = new Error();
+        const error = new AuthError({ code: AuthError.UNKNOWN, originalError });
+        expect(error).toHaveProperty('originalError', originalError);
+      });
+    });
+  });
+});
