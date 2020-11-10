9491f2236c5738a4820c64ebdbb3e1690b2b7fc4
Daniil Bratukhin <dantothefuture@gmail.com>
2020-11-10 14:30:47 +0300

    feat: add args validation to webhook verification helper

--- src/integration/verifyWebhookSignature.ts
+++ src/integration/verifyWebhookSignature.ts
@@ -1,5 +1,7 @@
 import * as crypto from 'crypto';
 
+import v8n from 'v8n';
+
 interface Webhook {
   /** The `Foxy-Webhook-Signature` header value received with the webhook. */
   signature: string;
@@ -9,6 +11,12 @@ interface Webhook {
   key: string;
 }
 
+const webhookV8N = v8n().schema({
+  key: v8n().string(),
+  payload: v8n().string(),
+  signature: v8n().string(),
+});
+
 /**
  * Verifies that the webhook your app has received was indeed sent from our servers.
  * See [our wiki](https://wiki.foxycart.com/v/2.0/webhooks#validating_the_payload) for more info.
@@ -17,6 +25,7 @@ interface Webhook {
  * @returns True if this webhook has a valid signature.
  */
 export function verifyWebhookSignature(webhook: Webhook): boolean {
+  webhookV8N.check(webhook);
   const computedSignature = crypto.createHmac('sha256', webhook.key).update(webhook.payload).digest('hex');
   return webhook.signature === computedSignature;
 }
