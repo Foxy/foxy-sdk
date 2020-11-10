d87865977ddce76168b2af238f906633a336c1d2
Daniil Bratukhin <dantothefuture@gmail.com>
2020-11-10 14:23:54 +0300

    docs: update webhook verification docs

--- src/integration/verifyWebhookSignature.ts
+++ src/integration/verifyWebhookSignature.ts
@@ -1,44 +1,22 @@
 import * as crypto from 'crypto';
 
-export interface VerificationParams {
-  /**
-   * The `Foxy-Webhook-Signature` header value received with the webhook.
-   *
-   * @see https://wiki.foxycart.com/v/2.0/webhooks
-   */
+interface Webhook {
+  /** The `Foxy-Webhook-Signature` header value received with the webhook. */
   signature: string;
-
-  /**
-   * The serialized (string) request body received with the webhook.
-   *
-   * @see https://wiki.foxycart.com/v/2.0/webhooks
-   */
+  /** The serialized (string) request body received with the webhook. */
   payload: string;
-
-  /**
-   * The encryption key for this particular webhook.
-   *
-   * @see https://wiki.foxycart.com/v/2.0/webhooks
-   */
+  /** The encryption key for this particular webhook. */
   key: string;
 }
 
 /**
  * Verifies that the webhook your app has received was indeed sent from our servers.
+ * See [our wiki](https://wiki.foxycart.com/v/2.0/webhooks#validating_the_payload) for more info.
  *
- * @example
- *
- * const isVerified = FoxyApi.webhook.verify({
- *   signature: "...",
- *   payload: "...",
- *   key: "..."
- * });
- *
- * @param params info received with the webhook that needs validation
- * @tutorial https://wiki.foxycart.com/v/2.0/webhooks#validating_the_payload
+ * @param webhook info received with the webhook that needs validation
+ * @returns True if this webhook has a valid signature.
  */
-export function verifyWebhookSignature(params: VerificationParams): boolean {
-  const computedSignature = crypto.createHmac('sha256', params.key).update(params.payload).digest('hex');
-
-  return params.signature === computedSignature;
+export function verifyWebhookSignature(webhook: Webhook): boolean {
+  const computedSignature = crypto.createHmac('sha256', webhook.key).update(webhook.payload).digest('hex');
+  return webhook.signature === computedSignature;
 }
