7795348fe73adea77b8b9abe2759cd07c4c6f7cb
Daniil Bratukhin <dantothefuture@gmail.com>
2020-12-01 01:00:00 +0300

    feat: add allowed frequencies generator utility

--- src/integration/index.ts
+++ src/integration/index.ts
@@ -5,6 +5,7 @@ export { removeSensitiveData } from './removeSensitiveData';
 export { removeAllLinksExcept } from './removeAllLinksExcept';
 export { removePrivateAttributes } from './removePrivateAttributes';
 export { verifyWebhookSignature } from './verifyWebhookSignature';
+export { getAllowedFrequencies } from './getAllowedFrequencies';
 export { createSSOURL } from './createSSOURL';
 
 import type * as Rels from './Rels';
