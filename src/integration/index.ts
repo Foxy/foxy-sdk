068c2e48c24c3e5f632ee3afa384bab744ac562c
Daniil Bratukhin <dantothefuture@gmail.com>
2020-12-01 01:30:22 +0300

    feat: remove traverse mappers
    These mappers were used internally in one of the legacy projects. No need for them to be included in the SDK.

--- src/integration/index.ts
+++ src/integration/index.ts
@@ -1,9 +1,5 @@
 export { API } from './API';
 export { Signer } from './Signer';
-export { removeProperties } from './removeProperties';
-export { removeSensitiveData } from './removeSensitiveData';
-export { removeAllLinksExcept } from './removeAllLinksExcept';
-export { removePrivateAttributes } from './removePrivateAttributes';
 export { verifyWebhookSignature } from './verifyWebhookSignature';
 export { getAllowedFrequencies } from './getAllowedFrequencies';
 export { isNextTransactionDate } from './isNextTransactionDate';
