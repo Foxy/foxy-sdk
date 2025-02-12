e2374c6f0882516212f898be905d7c10b9aa4138
Daniil Bratukhin <dantothefuture@gmail.com>
2025-02-11 21:23:55 -0300

    fix(backend): fix typedefs for user invitations
    Closes #63

--- src/backend/Graph/user_invitations.d.ts
+++ src/backend/Graph/user_invitations.d.ts
@@ -3,7 +3,7 @@ import type { Graph } from '../../core';
 import type { UserInvitation } from './user_invitation';
 
 export interface UserInvitations extends Graph {
-  curie: 'fx:user_accesses';
+  curie: 'fx:user_invitations';
   links: CollectionGraphLinks<UserInvitations>;
   props: CollectionGraphProps;
   child: UserInvitation;
