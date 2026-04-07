bdce64555844684849f3447763b95720de52a11d
Daniil Bratukhin <dan@pheekus.dev>
2026-04-07 15:28:20 +0000

    refactor: move the embed custom elements to the checkout repo

--- tests/checkout/index.test.ts
+++ tests/checkout/index.test.ts
@@ -4,17 +4,14 @@
 
 import * as Checkout from '../../src/checkout';
 
-import { API } from '../../src/checkout/api';
 import { HttpCheckoutAPI } from '../../src/checkout/implementations/http-api';
-import { MockCheckoutAPI } from '../../src/checkout/implementations/mock-api';
 
 describe('Checkout', () => {
-  it('exports API contract', () => {
-    expect(Checkout).toHaveProperty('API', API);
+  it('exports concrete API class as API', () => {
+    expect(Checkout).toHaveProperty('API', HttpCheckoutAPI);
   });
 
-  it('exports concrete API implementations', () => {
-    expect(Checkout).toHaveProperty('HttpCheckoutAPI', HttpCheckoutAPI);
-    expect(Checkout).toHaveProperty('MockCheckoutAPI', MockCheckoutAPI);
+  it('does not export MockCheckoutAPI', () => {
+    expect(Checkout).not.toHaveProperty('MockCheckoutAPI');
   });
 });
