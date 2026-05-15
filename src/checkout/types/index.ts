96130f822c9bd72a6cfbf041a98b56ed2918bae7
Daniil Bratukhin <dan@pheekus.dev>
2026-05-15 15:11:13 +0000

    wip: add paypal_platform payment options generation

--- src/checkout/types/index.ts
+++ src/checkout/types/index.ts
@@ -1,25 +1,29 @@
-export type { TemplateSet } from './TemplateSet';
-export type { Transaction } from './Transaction';
-export type { Session } from './Session';
-export type { Customer } from './Customer';
-export type { Shipment } from './Shipment';
-export type { Item, ItemOption } from './Item';
-export type { Tax, Coupon, GiftCard, Totals } from './Totals';
-export type { BillingAddress } from './BillingAddress';
-export type { Store } from './Store';
-export type { Message } from './Message';
-export type { Format } from './Format';
-export type { Display } from './Display';
-export type { APIJson } from './APIJson';
-export type { APIEventMap } from './APIEventMap';
-export type { GooglePaymentsClient } from './GooglePaymentsClient';
-export type { CustomFields } from './CustomFields';
-export type { CustomConfig } from './CustomConfig';
-export type { ExpressCheckoutType, ExpressCheckoutOption } from './ExpressCheckoutOption';
-export type { PaymentOption, StripeV2Gateway } from './PaymentOption';
+export type { TemplateSet } from "./TemplateSet";
+export type { Transaction } from "./Transaction";
+export type { Session } from "./Session";
+export type { Customer } from "./Customer";
+export type { Shipment } from "./Shipment";
+export type { Item, ItemOption } from "./Item";
+export type { Tax, Coupon, GiftCard, Totals } from "./Totals";
+export type { BillingAddress } from "./BillingAddress";
+export type { Store } from "./Store";
+export type { Message } from "./Message";
+export type { Format } from "./Format";
+export type { Display } from "./Display";
+export type { APIJson } from "./APIJson";
+export type { APIEventMap } from "./APIEventMap";
+export type { GooglePaymentsClient } from "./GooglePaymentsClient";
+export type { PayPalSdkInstance } from "./PayPalSdkInstance";
+export type { CustomFields } from "./CustomFields";
+export type { CustomConfig } from "./CustomConfig";
+export type {
+  ExpressCheckoutType,
+  ExpressCheckoutOption,
+} from "./ExpressCheckoutOption";
+export type { PaymentOption, StripeV2Gateway } from "./PaymentOption";
 export type {
   AchHostedFieldsPublicState,
   AchHostedFieldsTokenizeErrorCode,
   CardValidationField,
   CardEmbedTokenizeErrorCode,
-} from './HostedFields';
+} from "./HostedFields";
