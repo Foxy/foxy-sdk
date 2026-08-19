4b1a0f124161188297e0b4aafff601759c0081a4
Daniil Bratukhin <dan@pheekus.dev>
2026-08-19 18:54:20 -0300

    feat(core): add AuthError and ResolutionError
    Ported from v1 with v8n validators replaced by guards from core/guards.

---
+++ src/core/API/ResolutionError.ts
@@ -0,0 +1,16 @@
+/**
+ * This error is thrown when one of the requests for a resource
+ * in a path returns a non-2XX status code during the URL resolution process.
+ * If you're implementing a custom resolver, consider using this class
+ * to indicate resolution errors.
+ */
+export class ResolutionError extends Error {
+  /** API response object with a non-2XX status code. */
+  readonly response: Response;
+
+  constructor(response: Response) {
+    super();
+    if (!(response instanceof Response)) throw new TypeError('response must be an instance of Response.');
+    this.response = response;
+  }
+}
