1371793287811cb7ca9c0c0b56f2fac6501f96b5
Daniil Bratukhin <dan@pheekus.dev>
2026-08-21 20:44:01 -0300

    feat(admin): port createSSOURL and verifyWebhookSignature from v1 Backend

---
+++ src/admin/verifyWebhookSignature.ts
@@ -0,0 +1,25 @@
+import * as crypto from 'crypto';
+
+import { assertWebhookSignaturePayload } from '../core/guards.js';
+
+interface Webhook {
+  /** The `Foxy-Webhook-Signature` header value received with the webhook. */
+  signature: string;
+  /** The serialized (string) request body received with the webhook. */
+  payload: string;
+  /** The encryption key for this particular webhook. */
+  key: string;
+}
+
+/**
+ * Verifies that the webhook your app has received was indeed sent from our servers.
+ * See [our wiki](https://wiki.foxycart.com/v/2.0/webhooks#validating_the_payload) for more info.
+ *
+ * @param webhook info received with the webhook that needs validation
+ * @returns True if this webhook has a valid signature.
+ */
+export function verifyWebhookSignature(webhook: Webhook): boolean {
+  assertWebhookSignaturePayload(webhook);
+  const computedSignature = crypto.createHmac('sha256', webhook.key).update(webhook.payload).digest('hex');
+  return webhook.signature === computedSignature;
+}
